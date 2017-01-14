e4ccdd46cae27b94c8f9c0293a5879cb5d291481
Diego Freniche Brito <diego@freniche.com>
2017-01-14 17:53:25 +0100

    Entered map on Madrid, changed to SupportMapFragment

--- app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
+++ app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
@@ -7,6 +7,11 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import io.keepcoding.madridguide.R;
 import io.keepcoding.madridguide.fragments.ShopsFragment;
@@ -18,9 +23,13 @@ import io.keepcoding.madridguide.model.Shop;
 import io.keepcoding.madridguide.model.Shops;
 import io.keepcoding.madridguide.navigator.Navigator;
 import io.keepcoding.madridguide.util.OnElementClick;
+import io.keepcoding.madridguide.util.map.MapHelper;
 
 public class ShopsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    GoogleMap googleMap;
+
+    private MapFragment mapFragment;
     private ShopsFragment shopsFragment;
     private Shops shops;
 
@@ -29,7 +38,7 @@ public class ShopsActivity extends AppCompatActivity implements LoaderManager.Lo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
 
-        shopsFragment = (ShopsFragment) getFragmentManager().findFragmentById(R.id.activity_shops_fragment_shops);
+        shopsFragment = (ShopsFragment) getSupportFragmentManager().findFragmentById(R.id.activity_shops_fragment_shops);
 
         LoaderManager loaderManager = getSupportLoaderManager();
         // loaderManager.initLoader(0, null, this);
@@ -49,6 +58,35 @@ public class ShopsActivity extends AppCompatActivity implements LoaderManager.Lo
                 shopsFragment.setShops(shops);
             }
         });
+
+        setupMap();
+    }
+
+
+    private void initializeMap() {
+        if (googleMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            googleMap = mapFragment.getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setRotateGesturesEnabled(false);
+            }
+        }
+    }
+
+
+    private void setupMap() {
+        final float lat = 40.415363f;
+        final float lon = -3.707398f;
+
+        initializeMap();
+        MapHelper.centerMapInPosition(googleMap, lat, lon);
     }
 
     // Cursor Loaders using Content Provider
