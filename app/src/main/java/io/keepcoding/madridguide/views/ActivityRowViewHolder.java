d11847cf4861b288e52dfbba62877b5ff7c9d7c9
Diego Freniche Brito <diego@freniche.com>
2017-01-19 20:03:32 +0100

    Changed policy to load images from cache

--- app/src/main/java/io/keepcoding/madridguide/views/ActivityRowViewHolder.java
+++ app/src/main/java/io/keepcoding/madridguide/views/ActivityRowViewHolder.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 import java.lang.ref.WeakReference;
@@ -34,7 +35,7 @@ public class ActivityRowViewHolder extends RecyclerView.ViewHolder {
         this.nameTextView.setText(activity.getName());
         Picasso.with(context.get())
                 .load(activity.getLogoImgUrl())
-                //.networkPolicy(NetworkPolicy.OFFLINE)
+                .networkPolicy(NetworkPolicy.OFFLINE)
                 .placeholder(android.R.drawable.ic_btn_speak_now)
                 .into(logoImageView);
     }
